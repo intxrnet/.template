a179d34275bf0449359e37a20a619157806d632f
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2025-02-11 13:59:12 -0500

    REM sequenec

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
 import type { NextConfig } from "next";
-import { fetchCloudData } from "./assets";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
-  onBuild: async () => {
-    try {
-      await fetchCloudData();
-    } catch (error) {
-      console.error("Failed to fetch cloud data:", error);
-      // Optionally throw error to fail build
-      // throw error;
-    }
-  },
 };
 
 export default nextConfig;
